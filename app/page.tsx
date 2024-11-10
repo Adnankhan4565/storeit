0945c478d6520afba7edee46b93a650048ec4640
Adnan khan <IGNITION420@GMAIL.COM>
2024-11-10 11:52:02 +0500

    auth pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center bg-amber-100">
+    <div className="">
       <h1 className="text-center text-4xl font-bold">Welcome to Storeit</h1>
     </div>
   );
